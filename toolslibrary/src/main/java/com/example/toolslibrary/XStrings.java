048b27e2926560b29ee346715429470e72745c85
chenyu <1429911058@qq.com>
2018-08-22 16:01:10 +0800

    第一次

---
+++ toolslibrary/src/main/java/com/example/toolslibrary/XStrings.java
@@ -0,0 +1,11 @@
+package com.example.toolslibrary;
+
+/**
+ * Created by Administrator on 2018/8/22 0022.
+ */
+
+public class XStrings {
+    public static boolean isEmpty(String string) {
+        return string == null || (string.trim().length() <= 0);
+    }
+}
